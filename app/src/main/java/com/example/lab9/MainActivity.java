449a74e680d461a1a2c998d2a077050c16b74753
Kirill <yourmail@mail.ru>
2024-01-05 18:18:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lab9/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.lab9;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login_btn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login_btn = (Button) findViewById(R.id.login_btn);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = gsc.getSignInIntent();
+                GoogleSignInLauncher.launch(intent);
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> GoogleSignInLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+            try {
+                task.getResult(ApiException.class);
+                Toast toast = Toast.makeText(getApplicationContext(), "Вход успешен", Toast.LENGTH_LONG);
+                toast.show();
+
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+                if (account != null) {
+                    String name = account.getDisplayName();
+                    String email = account.getEmail();
+                    Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                    i.putExtra("name", name);
+                    i.putExtra("email", email);
+                    startActivity(i);
+                }
+
+            } catch (ApiException e) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Ошибка входа", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    });
+}
---
+++ app/src/main/java/com/example/lab9/MainActivity2.java
@@ -0,0 +1,55 @@
+package com.example.lab9;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private Button logout_btn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.get("name").toString();
+        String email = arguments.get("email").toString();
+        logout_btn = (Button) findViewById(R.id.logout_btn);
+        TextView user_text = (TextView) findViewById(R.id.textView_name);
+        user_text.setText(name);
+
+        TextView email_text = (TextView) findViewById(R.id.textView_email);
+        email_text.setText(email);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
+            }
+        });
+    }
+}
